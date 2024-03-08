4af231ffba7c1f215515b57adcbf9d8af1791fe1
odwiss42 <olivier.doubre@gmail.com>
2024-03-08 15:28:27 +0100

    Added inline_markdown functionalities

--- src/main.py
+++ src/main.py
@@ -3,6 +3,23 @@ from htmlnode import (
     LeafNode,
     ParentNode)
 
+from textnode import (
+    TextNode,
+    text_type_text,
+    text_type_bold,
+    text_type_italic,
+    text_type_code,
+    text_type_image,
+    text_type_link,
+)
+
+from inline_markdown import (
+    split_nodes_delimiter,
+    extract_markdown_images,
+    extract_markdown_links
+)
+
+
 
 def main():
     node1 = ParentNode(
@@ -32,5 +49,14 @@ def main():
     nested_node = ParentNode("p",[node1,node2,node3])
 
     print(nested_node.to_html())
+
+    txt_node = TextNode("This is an image", text_type_image, 'https://www.boot.dev/boots.jpg')
+    print(txt_node.text_node_to_html_node())
+
+    text = "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and ![another](https://i.imgur.com/dfsdkjfd.png)"
+    print(extract_markdown_images(text))
+
+    text = "This is text with a [link](https://www.example.com) and [another](https://www.example.com/another)"
+    print(extract_markdown_links(text)) 
     
 main()
